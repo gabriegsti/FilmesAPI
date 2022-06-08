a08acd0c7dc6b9c6a61abe55d8376734501eca1d
gabriegsti <gabrielgsdevbr@gmail.com>
2022-06-08 14:39:08 -0300

    implementação usario api

---
+++ UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto createDto)
+        {
+            // todo chamar o service
+            return Ok();
+        }
+
+
+    }
+}
